acdc606831d5137f31868a2fa5639d62d8bfd127
Noe-Delcroix <noe.delcroix59@gmail.com>
2023-12-15 09:48:32 +0100

    meilleur gestion du user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/auth/info', {withCredentials:true}); // Adjust the URL as needed
+                setUserInfo(response.data);
+            } catch (error) {
+                console.error('Error fetching user info:', error);
+                // Handle error (e.g., redirect to login if not authenticated)
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
 
     const handleLogout = () => {
+        // Implement the logout logic (e.g., clearing session and redirecting)
         navigate('/login');
-    }
-
+    };
 
     return (
-        <nav className="bg-blue-500 text-white p-4">
-            <h1 className="text-lg font-bold">CodePulse</h1>
-            <a onClick={handleLogout}>Logout</a>
+        <nav className="bg-blue-500 text-white p-4 flex flex-row justify-between items-center">
+            <div>
+                <h1 className="text-3xl font-bold ">CodePulse</h1>
+            </div>
+
+            {userInfo && (
+                <div className='flex flex-row items-center'>
+                    <div className='flex flex-col items-end'>
+                        <span className='font-bold'>{`Logged as ${userInfo.name}`}</span>
+                        <button onClick={handleLogout}>Logout</button>
+                    </div>
+                    <img src={userInfo.picture} alt="Avatar" className="w-10 h-10 rounded-full ml-3" />
+                </div>
+            )}
+
+
         </nav>
     );
 };
